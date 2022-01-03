97b868fb6836ccb74d82e49d03291cb54e0db475
mikana0918 <shogo.kusuhara@gmail.com>
2022-01-03 15:39:52 +0900

    refactor: :recycle: refactor random word to create one widget

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,25 @@
 // found in the LICENSE file.
 
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() {
   runApp(const MyApp());
+}
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({Key? key}) : super(key: key);
+
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordpair = WordPair.random();
+    return Text(wordpair.asPascalCase);
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -13,6 +29,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
@@ -20,7 +37,7 @@ class MyApp extends StatelessWidget {
           title: const Text('Welcome to Flutter'),
         ),
         body: const Center(
-          child: Text('Hello World'),
+          child: RandomWords(),
         ),
       ),
     );
